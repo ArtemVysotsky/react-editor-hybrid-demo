8e3cf2942e4aaacd9096d5402dc5a7836898ecab
Artem Vysotsky <a.vysotsky@gmail.com>
2024-11-15 00:03:35 +0200

    add src folder

---
+++ src/config.js
@@ -0,0 +1,23 @@
+export default {
+    blocks: {
+        count: 7,
+        list: {
+            text: 'Текст', image: 'Зображення', header: 'Підзаголовок',
+            video: 'Відео', facebook: 'Фейсбук', twitter: 'Твіттер', map: 'Мапа',
+            table: 'Таблиця', list: 'Перелік', quote: 'Цитата', raw: 'Неформат',
+            address: 'Адреса', sound: 'Звук', code: 'Код', html: 'HTML'
+        }
+    },
+    highlight: {
+        languages: {
+            count: 5,
+            list: {
+                txt: 'Text', xml: 'XML', css: 'CSS', html: 'HTML', json: 'JSON',
+                yaml: 'YAML', cpp: 'C++', java: 'Java', python: 'Python',
+                javascript: 'JavaScript', typescript: 'TypeScript',
+                perl: 'Perl', php: 'PHP'
+            }
+        }
+    },
+    tinymce: '/tinymce/tinymce.min.js'
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Post from './pages/Post/Editor.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+)
+
+root.render(
+    <React.StrictMode><Post /></React.StrictMode>
+)
