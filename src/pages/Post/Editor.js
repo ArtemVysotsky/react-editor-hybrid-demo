69c40b5136b9211a1c61243a11f91e905305936c
Artem Vysotsky <a.vysotsky@gmail.com>
2024-11-15 14:39:33 +0200

    add pages

---
+++ src/pages/Post/Editor.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useReducer } from 'react'
+import Editor, { Reducer, actions} from '../../components/Editor.js'
+import Main from './Editor/Main.js'
+
+// For demo version only!!!
+const objectSetValue = (data, name, value) => {
+    const keys = name.split('.')
+    const dataNew = { ...data }
+    let current = dataNew
+    keys.forEach((key, index) => {
+      if (index === keys.length - 1) {
+        current[key] = value
+      } else {
+        current[key] = current[key] || {}
+        current = current[key]
+      }
+    })
+    return dataNew
+}
+
+export default () => {
+
+    const [post, setPost] = useState({
+        title: '', description: ''
+    })
+    const [state, dispatch] = useReducer(Reducer)
+
+    const handleChange = (name, value) => {
+        console.log(name, value)
+        let postNew
+        if (typeof value !== undefined) {
+            postNew = objectSetValue(post, name, value )
+        } else {
+            let { [name]: value, ...postNew } = post
+        }
+        console.log(postNew)
+        setPost(postNew)
+    }
+
+    // For demo version only!!!
+    const handleDownload = event => {
+        let postNew = { ...post }
+        if (state?.length > 0) {
+            postNew.blocks = state
+        } else {
+            delete postNew.blocks
+        }
+        console.log('post.download', postNew)
+        const json = JSON.stringify(postNew, null, 2);
+        const blob = new Blob([json], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        event.target.setAttribute('download', 'post.json')
+        event.target.href = url
+    }
+
+    /*
+    const handleSubmit = async () => {
+        let postNew = { ...post }
+        if (state?.length > 0) {
+            postNew.blocks = state
+        } else {
+            delete postNew.blocks
+        }
+        setPost(postNew)
+        console.log('post.submit', postNew)
+        // ...
+    }
+    */
+
+    const handleDelete = async () => {
+        console.log('post.delete')
+        // ...
+    }
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('/post.json')
+            if (response.status !== 200) {
+                throw Error(response.statusText)
+            }
+            const postNew = await response.json()
+            console.log('post.get', postNew)     
+            dispatch(
+                actions.load(postNew.blocks ?? [])
+            )
+            setPost(postNew)
+        })()
+    }, [])
+
+    return <div>
+        <article itemScope="itemscope" itemType="https://schema.org/Article">
+            <Main title={post.title} image={post.image} onChange={handleChange} />
+            <Editor blocks={{ state, dispatch, actions }} intro={post.description}
+                onChangeIntro={intro => handleChange('description', intro)} />
+        </article>
+        <p className="text-center mt-5">
+            <a className="btn btn-primary me-2" onClick={handleDownload}
+                title="Зберегти публікацію">Зберегти</a>
+            <a className="btn btn-danger" onClick={handleDelete}
+                title="Видалити публікацію">Видалити</a>
+        </p>
+    </div>
+}
---
+++ src/pages/Post/Editor/Main.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useRef } from 'react'
+import { Button } from 'react-bootstrap'
+import Field from '../../../components/Editor/Field.js'
+
+export default (props) => {
+
+    const ref = useRef()
+
+    // For demo version only!!!
+    const handleChangeImage = () => {
+        const result = prompt(
+            'Вставте посилання на зображення',
+            props.image?.url
+        )
+        props.onChange('image.url', result)
+    }
+
+    useEffect(() => ref.current.focus(), [ref.current])
+
+    return <div className="block block-type-main">
+            <div className={'main' + (props.image?.url ? ' image' : '')}
+            style={{ backgroundImage: props.image?.url
+                ? `url(${props.image.url})` : ''}}>
+            <div className="layout">
+                <div className="head"></div>
+                <div className="body">
+                    <h1 contentEditable="true" suppressContentEditableWarning="true"
+                        onBlur={
+                            event => props.onChange('title', event.target.textContent)
+                        }
+                        onPaste={props.onPaste} ref={ref} className="editable">
+                        {props.title}
+                    </h1>
+                </div>
+                <div className="foot"></div>
+            </div>
+            <div className="bottom">
+                <Field name="image.source" value={props.image?.source}
+                    title="Джерело зображення" className="source flex-grow-1 text-start w-25"
+                    onChange={(name, value) => props.onChange(name, value)} />
+                <div className="buttons flex-grow-0">
+                    {props.image?.url
+                        ? <Button variant="danger" onClick={() => props.onChange('image.url')}
+                            title="Видалити зображення">Видалити зображення</Button>
+                        : <Button variant="success" onClick={handleChangeImage}
+                            title="Вставити зображення">Вставити зображення</Button>
+                    }
+                </div>
+                <Field name="image.description" value={props.image?.description}
+                    title="Опис зображення" className="description flex-grow-1 text-end w-25"
+                    onChange={(name, value) => props.onChange(name, value)} />
+            </div>
+        </div>
+    </div>
+}
